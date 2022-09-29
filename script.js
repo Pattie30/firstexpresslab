c4a924895c550af3e58e4d91c5a94a94525ca747
Pattie30 <87343336+Pattie30@users.noreply.github.com>
2022-09-29 16:28:27 -0400

    first activity

---
+++ script.js
@@ -0,0 +1,81 @@
+const express = require('express')
+
+const app = express()
+
+const name = ['Patricia'];
+
+app.get('/', function (req, res){ 
+    console.log(req.params);
+    res.send   (`Hiya!`)
+   
+})
+
+app.get('/greeting', function (req, res){ 
+    res.send   (`What is up!`)
+   
+})
+
+
+
+app.get('/greeting/:name', (req, res) => { 
+    const {name} = req.params
+    console.log(req.params);
+    console.log(req.query);
+
+    for (let client of name){
+        console.log(client);
+        if(client === name) {
+            return res.send(`Hello ${name}! Its so great to see you!`)
+        }else{
+            console.log('Client Not Found!');
+        }
+    }
+    res.send   ('Client Not Found!')
+   
+})
+    
+
+
+
+
+app.listen(3000, function(){
+    console.log('Listening on Port 3000')
+} )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
