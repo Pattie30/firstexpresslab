9e1333b28ac12fa010c740ecc6f8c1a08c27d237
Pattie30 <87343336+Pattie30@users.noreply.github.com>
2022-10-01 15:30:19 -0400

    Magic 8 Ball express application created

--- script.js
+++ script.js
@@ -60,10 +60,23 @@ app.get('/tip/:total/:percentage', function(req, res) {
 
 
 
-    app.listen(3000, function(){
-        console.log('Listening on Port 3000')
-    } )
+  
     
+//************************ MAGIC 8 BALL **********************************************************/
+
+const response = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely",
+ "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+ "Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+ app.get('/magic', function (req, res){ 
+    const {magic} = req.params
+    res.send   (`MAGIC 8 BALL`)   
+})
+app.get('/magic/:question', function (req, res){ 
+    const {magic, question} = req.params
+    const randomFrase = response[Math.floor(Math.random() * response.length)]
+    res.send   (`MAGIC 8 BALL RESPONSE: ${randomFrase}`)   
+})
 
 
 
@@ -92,3 +105,6 @@ app.get('/tip/:total/:percentage', function(req, res) {
 
 
 
+app.listen(3000, function(){
+    console.log('Listening on Port 3000')
+} )
