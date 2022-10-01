abe1b611f011567b5bebf6971dd29cb77172a36d
Pattie30 <87343336+Pattie30@users.noreply.github.com>
2022-10-01 15:11:43 -0400

    Tip Calculator express application created

--- script.js
+++ script.js
@@ -1,47 +1,69 @@
-const express = require('express')
+//****************** GREETING EXPRESS APP ************************************/
+ const express = require('express')
 
-const app = express()
+ const app = express()
 
-const name = ['Patricia'];
+//const name = ['Patricia'];
+
+//& app.get('/', function (req, res){ 
+// &    console.log(req.params);
+// &    res.send   (`Hiya!`)
+   
+//& })
+
+//& app.get('/greeting', function (req, res){ 
+// &    res.send   (`What is up!`)
+   
+//& })
+
+//& app.get('/greeting/:name', (req, res) => { 
+// &    const {name} = req.params;
+  
+// &             res.send(`Hello ${name}! Its so great to see you!`)
+    
+//& })
+//& app.listen(3000, function(){
+// &    console.log('Listening on Port 3000')
+// &} )
+
+//*************************** TIP CALCULATOR ****************************************** */
+
+// const total = ''
+// const percentage = [total / hundred];
 
 app.get('/', function (req, res){ 
-    console.log(req.params);
-    res.send   (`Hiya!`)
-   
-})
-
-app.get('/greeting', function (req, res){ 
-    res.send   (`What is up!`)
-   
+      console.log(req.params);
+      res.send   (`Hiya!`)
 })
 
 
 
-app.get('/greeting/:name', (req, res) => { 
-    const {name} = req.params;
-  // console.log(req.params);
-  // console.log(req.query);
+app.get('/tip', function (req, res){ 
+    const {tip} = req.params
+    res.send   (`Tip calculator`)
+   
+})
 
-    // for (let client of name){
-    //     console.log(client);
-    //     if(client === name) {
-             res.send(`Hello ${name}! Its so great to see you!`)
-        
+app.get('/tip/:total', function(req, res){
+    const {total} = req.params;
+     res.send (`Your total is: ${total}`)
+})
+
+app.get('/tip/:total/:percentage', function(req, res) {
+    const {total,percentage} = req.params;
+    const hundred = '100';
+    //const {total} = req.params;
+     const endResult = total / hundred * percentage
+      res.send(`Your tip is ${endResult}`)
+
+})
+
+
+
+    app.listen(3000, function(){
+        console.log('Listening on Port 3000')
+    } )
     
-  //  res.send   ('Client Not Found!')
-   
-    
-})
-
-
-
-
-app.listen(3000, function(){
-    console.log('Listening on Port 3000')
-} )
-
-
-
 
 
 
